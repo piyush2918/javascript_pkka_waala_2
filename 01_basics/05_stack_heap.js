43f5185190b411b2060b6be131d1119962d6c8ed
piyush2918 <137400037+piyush2918@users.noreply.github.com>
2024-05-23 10:47:27 +0000

    stack and heaps

---
+++ 01_basics/05_stack_heap.js
@@ -0,0 +1,22 @@
+//stack(primitive)
+//heap (Non-primitive)
+
+let myYoutubename = "hiteshchaudharydotcom"
+
+let anothername = myYoutubename
+anothername = "chaiaurcode"
+
+console.log(myYoutubename)
+console.log(anothername);
+
+let userOne = {
+      email: "user@google.com"
+      
+}
+
+let userTwo = userOne
+
+userTwo.email = "hitesh@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
