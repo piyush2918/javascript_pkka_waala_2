f833bda3e18a84d9110850e528a8c226e724856f
piyush2918 <137400037+piyush2918@users.noreply.github.com>
2024-05-23 09:52:31 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -24,5 +24,41 @@ let boolenIsLoggedIn = Boolean(isLoggedIn)
 let somenumber= 33
 
 let stringNumber = String(somenumber)
-console.log(stringNumber)
-console.log(typeof stringNumber);
+//console.log(stringNumber)
+//console.log(typeof stringNumber);
+
+
+//********************************************* OPERATIONS *******************************************************************//
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**3);
+//console.log(2/3);
+//console.log(2%3);
+
+let str1 = "hello"
+let str2 = " piyush"
+
+let str3 = str1+str2
+//console.log(str3);
+
+//console.log("1" + 2);
+//console.log(1 + "2");
+//console.log("1" +2+2);
+//console.log(1+2+"2");
+
+//console.log((3+4) *5 %3);
+
+//console.log(true);
+
+let num1 , num2 , num3
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
