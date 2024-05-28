4f30d662e541871495bb14e538295f422c8808ee
piyush2918 <137400037+piyush2918@users.noreply.github.com>
2024-05-28 10:31:57 +0000

    controlflow

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,10 @@
+//Immediately invoked function expressions (IIFE)
+
+(function chai(){
+    //named IIFE
+    console.log(`DB CONNECTED`);
+})();
+
+( (name) => {
+    console.log(`DB CONNECTED TWO ${name}`);
+})('hitesh')
