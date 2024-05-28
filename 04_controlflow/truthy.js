999669188e51e769c1b31298531cab198d9d0a08
piyush2918 <137400037+piyush2918@users.noreply.github.com>
2024-05-28 11:25:32 +0000

    truthy

---
+++ 04_controlflow/truthy.js
@@ -0,0 +1,40 @@
+//const userEmail = ""=>else , []=>if , "hitesh"=>if
+
+if(userEmail){
+    console.log("got user email");
+}
+else{
+    console.log("dont have user email");
+}
+
+//falsy values
+//false , 0 ,-0 , BigInt 0n , "" , null , undefined , NaN
+
+//truthy values
+//"0" , "false" , " " , [] ,{} , function(){}
+
+if(userEmail.length === 0){
+    console.log("Array is empty");
+}
+
+const emptyObj = {}
+
+if(Object.keys(emptyObj).length === 0){
+    console.log("Object is empty");
+}
+
+//Nullish Coalescing Operator (??): null undefined
+
+let val1;
+//val1 = 5 ?? 10
+//val1 = null ?? 10
+//val1 = undefined ?? 15
+// val1 = null ?? 10 ?? 20
+
+console.log(val1);
+
+//Terniary Operator
+
+//condition ? true : false
+const icetea = 100
+iceteaprice <= 80 ? console.log("less than 80"): console.log("more than 80")
